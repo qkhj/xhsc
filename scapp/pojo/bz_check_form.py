ecd5db85c2795e92797c8c393b74e3f67a39f8d8
qkhj <qk_hejia@163.com>
2014-03-02 22:59:00 -0800

    [modify]标准检测表单

---
+++ scapp/pojo/bz_check_form.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+class CheckForm:
+    #定义基本属性
+    jkr=0
+    khjl=0
+    khId=0
+    htId=0
+    dkje=0
+    jjId=0
+    fkDate=0
+    dqDate=0
+    lv=0
+    hkqs=0
+    hkfs=0
+    def __init__(self):
+        self.jkr=0
+        self.khjl=0
+        self.khId=0
+        self.htId=0
+        self.dkje=0
+        self.jjId=0
+        self.fkDate=0
+        self.dqDate=0
+        self.lv=0
+        self.hkqs=0
+        self.hkfs=0
