9fee80f36bce2500eb711b6a85feced57a9d3f25
qkhj <qk_hejia@163.com>
2014-03-06 19:04:17 -0800

    [modify]固定资产批量新增

--- scapp/models/credit_data/sc_fixed_assets_car.py
+++ scapp/models/credit_data/sc_fixed_assets_car.py
@@ -12,29 +12,28 @@ class SC_Fixed_Assets_Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     loan_apply_id=db.Column(db.Integer)
     name=db.Column(db.String(32))#车辆名称
-    owner=db.Column(db.String(32))#持有人
-    type_brand=db.Column(db.String(32))#型号或品牌
+    # owner=db.Column(db.String(32))#持有人
+    # type_brand=db.Column(db.String(32))#型号或品牌
     purchase_date=db.Column(db.Date)#购置时间
-    purchase_price=db.Column(db.String(32))#购置价格
-    production_date=db.Column(db.Date)#生产时间
+    purchase_price=db.Column(db.String(32))#原始单价
+    # production_date=db.Column(db.Date)#生产时间
     total_price= db.Column(db.String(32))#总价
-    outward=db.Column(db.String(64))#外观评价
-    remark=db.Column(db.String(64))#备注
+    # outward=db.Column(db.String(64))#外观评价
+    # remark=db.Column(db.String(64))#备注
+    rate =db.Column(db.Integer)#折旧率
+    total =db.Column(db.Integer)#数量
+    rate_price=db.Column(db.Integer)#折旧后价值
 
-
-    def __init__(self,loan_apply_id,name,owner,type_brand,purchase_date,purchase_price,production_date,total_price,
-                 outward,remark):
+    def __init__(self,loan_apply_id,name,purchase_date,purchase_price,rate,total,total_price,
+                 rate_price):
         self.loan_apply_id = loan_apply_id
         self.name = name
-        self.owner = owner
-        self.type_brand = type_brand
         self.purchase_date = purchase_date
         self.purchase_price = purchase_price
-        self.production_date = production_date
         self.total_price = total_price
-        self.outward = outward
-        self.remark = remark
-
+        self.rate = rate
+        self.total = total
+        self.rate_price = rate_price
 
     def add(self):
         db.session.add(self)
