92850a47dceae9fe1a9008c647220dd3b825ab11
johhny <jianghanshi@vip.qq.com>
2014-04-09 11:12:48 +0800

    [mod] 贷款还款信息重做初始版本

--- scapp/tools/convert_bank_data/Interface_bank_data.py
+++ scapp/tools/convert_bank_data/Interface_bank_data.py
@@ -5,7 +5,7 @@
 __author__ = 'johhny'
 from repayment import DAO_repayment
 from overdue import DAO_overdue
-
+from bank_loans import DAO_bank_loans
 #DAO_repayment
 
 
@@ -54,16 +54,16 @@ def update_rep(loan_apply_id,tran_no,last_repayment_day,crnt_pr,arfn_pr,crnt_int
     @param id: id
     @return:None
     """
-    return update_rep(loan_apply_id,tran_no,last_repayment_day,crnt_pr,arfn_pr,crnt_int,arfn_int,id)
+    return DAO_repayment.update_rep(loan_apply_id,tran_no,last_repayment_day,crnt_pr,arfn_pr,crnt_int,arfn_int,id)
 
 
 
-def cal_last_month_intrest():
+def insert_last_month_intrest():
     """
     计算上个月利润贡献,插入sc_sta_mlm表中
     @return:None
     """
-    return DAO_repayment.cal_last_month_intrest()
+    return DAO_repayment.insert_last_month_intrest()
 
 #DAO_overdue
 
@@ -86,3 +86,62 @@ def update_overdue_rate():
     """
     return DAO_overdue.update_overdue_rate()
 
+
+#DAO_bank_loans
+
+def insert_bank_loans_info(loan_apply_id,loan_account,
+                 loan_status,loan_total_amount,loan_balance,loan_deliver_date,loan_due_date,
+                 loan_closed_date,loan_cleared_pr_n,loan_cleared_in_n,loan_overdue_amount,loan_overdue_date):
+    """
+    新增银行贷款信息
+    @param loan_apply_id:
+    @param loan_account: 贷款账号
+    @param loan_status: 贷款状态
+    @param loan_total_amount: 贷款总额
+    @param loan_balance: 贷款余额
+    @param loan_deliver_date: 放款日期
+    @param loan_due_date: 到期日
+    @param loan_closed_date: 结清日
+    @param loan_cleared_pr_n: 已还本金期数
+    @param loan_cleared_in_n: 已还利息期数
+    @param loan_overdue_amount: 逾期本金
+    @param loan_overdue_date: 逾期日
+    @return: None
+    """
+    return DAO_bank_loans.insert_bank_loans_info(loan_apply_id,loan_account,
+                 loan_status,loan_total_amount,loan_balance,loan_deliver_date,loan_due_date,
+                 loan_closed_date,loan_cleared_pr_n,loan_cleared_in_n,loan_overdue_amount,loan_overdue_date)
+
+
+
+def update_bank_loans_info(id,loan_apply_id,loan_account,
+                 loan_status,loan_total_amount,loan_balance,loan_deliver_date,loan_due_date,
+                 loan_closed_date,loan_cleared_pr_n,loan_cleared_in_n,loan_overdue_amount,loan_overdue_date):
+    """
+    更新银行贷款信息
+    @param id: 主键ID
+    @param loan_apply_id:
+    @param loan_account: 贷款账号
+    @param loan_status: 贷款状态
+    @param loan_total_amount: 贷款总额
+    @param loan_balance: 贷款余额
+    @param loan_deliver_date: 放款日期
+    @param loan_due_date: 到期日
+    @param loan_closed_date: 结清日
+    @param loan_cleared_pr_n: 已还本金期数
+    @param loan_cleared_in_n: 已还利息期数
+    @param loan_overdue_amount: 逾期本金
+    @param loan_overdue_date: 逾期日
+    @return: None
+    """
+    return DAO_bank_loans.update_bank_loans_info(id,loan_apply_id,loan_account,
+                 loan_status,loan_total_amount,loan_balance,loan_deliver_date,loan_due_date,
+                 loan_closed_date,loan_cleared_pr_n,loan_cleared_in_n,loan_overdue_amount,loan_overdue_date)
+
+
+def get_need_update_bli():
+    """
+    获取需要更新的银行贷款信息id
+    @return:需要更新的银行贷款信息id与loan_apply_id
+    """
+    return DAO_bank_loans.get_need_update()
--- scapp/tools/convert_bank_data/overdue/DAO_overdue.py
+++ scapp/tools/convert_bank_data/overdue/DAO_overdue.py
@@ -20,69 +20,10 @@ def get_is_overdue(loan_apply_id,installments):
         if now>repayment_date:
             return True
         else:
-            insert_overdue(loan_apply_id,installments)
             return False
     else:
         return False
 
 
-#有逾期向客户经理每月信息统计表中插入逾期数据
-def insert_overdue(loan_apply_id,installments):
-    overdue_amount=get_overdue_amount(loan_apply_id,installments)
-    user_id=assist.get_user_id_by_lai(loan_apply_id)
-    #获取当前时间并转换为%Y-%m格式 用来与数据库中数据匹配
-    now=datetime.datetime.now()
-    month=datetime.datetime.strftime(now,'%Y-%m')
-    UPDATE_STR="UPDATE sc_sta_mlm set overdue_amount=%s WHERE " \
-                       "user_id=%s AND month=%d"%(overdue_amount,user_id,month)
-    logger.info(UPDATE_STR)
-    trans_local=local_db_conn.begin()
-    try:
-        local_db_conn.execute(UPDATE_STR)
-        trans_local.commit()
-    except():
-        logger.error("error")
-        trans_local.rollback()
-
-
-#取得逾期金额
-def get_overdue_amount(loan_apply_id,installments):
-    QUERY_STR="SELECT (srd.total-sr.total_repayment) AS overdue_amount " \
-              "FROM sc_repayment_plan_detail AS srd,sc_repayment AS sr " \
-              "WHERE srd.loan_apply_id=%s AND sr.loan_apply_id=%s AND " \
-              "srd.repayment_installments=%s AND sr.repayment_installments=%s"%(loan_apply_id,loan_apply_id,installments,installments)
-    return local_db_conn.execute(QUERY_STR).fetchall()[0]['overdue_amount']
-
-
-#每日统计当前所有人逾期比率
-def update_overdue_rate():
-    #获取所有已逾期的贷款编号
-    QUERY_STR_OD="SELECT COUNT(*) AS overdue,sla.A_loan_officer,sr.repayment_isntallments FROM sc_repayment AS sr INNER JOIN sc_loan_apply AS sla " \
-                  "ON sr.loan_apply_id=sla.id WHERE sr.status='2'"
-    data_OD=local_db_conn.execute(QUERY_STR_OD).fetchall()
-    if len(data_OD)>0:
-        for obj in data_OD:
-            #计算逾期率
-            hold_amount=get_hold_amount_by_id(obj['A_loan_officer'])
-            overdue_rate=obj['overdue']/hold_amount
-            #获取当前时间并转换为%Y-%m格式 用来与数据库中数据匹配
-            now=datetime.datetime.now()
-            month=datetime.datetime.strftime(now,'%Y-%m')
-            UPDATE_STR="UPDATE sc_sta_mlm set overdue_rate=%s WHERE " \
-                       "user_id=%s AND month=%d"%(overdue_rate,obj['A_loan_officer'],month)
-            logger.info(UPDATE_STR)
-            trans_local=local_db_conn.begin()
-            try:
-                local_db_conn.execute(UPDATE_STR)
-                trans_local.commit()
-            except():
-                logger.error("error")
-                trans_local.rollback()
-
-#获得用户当前所拥有的所有管户数
-def get_hold_amount_by_id(user_id):
-    QUERY_STR="SELECT valid_sum,month FROM sc_performance_list WHERE manager_id=%s " \
-              "AND date_format(month,'%Y-%m')=date_format(now(),'%Y-%m')"%(user_id)
-    return local_db_conn.execute(QUERY_STR).fetchall()[0]['valid_sum']
 
 
