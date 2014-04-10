1e296064a946e9ec928952ce0b7e03122ee79027
tanwh <qk_tanwh@163.com>
2014-04-10 14:03:49 +0800

    [mod]评估搜索

--- scapp/logic/cust_mgr/sc_kpi.py
+++ scapp/logic/cust_mgr/sc_kpi.py
@@ -4,7 +4,8 @@ from flask.ext.login import current_user
 from scapp.models import SC_UserRole
 
 #获取客户经理及后台岗的培训期最终考核
-def get_user_kpi_train_final():
+#manager 客户经理id
+def get_user_kpi_train_final(manager):
 	userrole=SC_UserRole.query.filter_by(user_id=current_user.id).first()
 	level=userrole.role.role_level
 
@@ -13,14 +14,17 @@ def get_user_kpi_train_final():
 		sql = "select sc_user.user_id,real_name,score_1,score_2,score_3,score_4,avg_score from "
 		sql += "(SELECT sc_user.id AS user_id,sc_role.role_level,sc_user.real_name FROM sc_userrole "
 		sql += "INNER JOIN sc_role ON sc_userrole.role_id = sc_role.id INNER JOIN sc_user ON sc_user.id = sc_userrole.user_id "
-		sql += "where sc_role.role_level >= 2)sc_user "
+		if manager == '0':
+			sql += "where sc_role.role_level >= 2)sc_user "
+		else:
+			sql += "where sc_user.id = "+str(manager)+")sc_user "
 		sql += "left join sc_kpi_train_final on sc_user.user_id = sc_kpi_train_final.user_id"
 	#客户经理和后台岗只能看到自己的
 	else:
 		sql = "select sc_user.user_id,real_name,score_1,score_2,score_3,score_4,avg_score from "
 		sql += "(SELECT sc_user.id AS user_id,sc_role.role_level,sc_user.real_name FROM sc_userrole "
 		sql += "INNER JOIN sc_role ON sc_userrole.role_id = sc_role.id INNER JOIN sc_user ON sc_user.id = sc_userrole.user_id "
-		sql += "where sc_user = "+current_user.id+")sc_user "
+		sql += "where sc_user.id = "+str(current_user.id)+")sc_user "
 		sql += "left join sc_kpi_train_final on sc_user.user_id = sc_kpi_train_final.user_id"
 
 	return db.engine.execute(sql).fetchall()
